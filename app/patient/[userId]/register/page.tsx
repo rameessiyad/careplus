db1297a78bc152aacd1247a2cf26fd7e762ea96b
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-01-04 22:02:09 +0530

    register form file created

--- app/patient/[userId]/register/page.tsx
+++ app/patient/[userId]/register/page.tsx
@@ -1,20 +1,44 @@
 "use client";
+import RegisterForm from "@/components/forms/RegisterForm";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Register = () => {
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+      {/* TODO OTP verification | PasskeyModal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
-            src="/assets/icons/logo-icon.svg"
+            src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
             alt="logo"
+            className="mb-12 h-10 w-fit"
           />
+
+          <RegisterForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="text-dark-600 justify-items-end xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[390px]"
+      />
     </div>
   );
 };
---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../CustomFormField";
+import SubmitBtn from "../SubmitBtn";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  CHECKBOX = "checkbox",
+  PHONE_INPUT = "phoneInput",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patient/${newUser.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Shedule your first appointment</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="User"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="Email"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone Number"
+          placeholder="9123456789"
+        />
+
+        <SubmitBtn isLoading={isLoading} className="">
+          Get started
+        </SubmitBtn>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
